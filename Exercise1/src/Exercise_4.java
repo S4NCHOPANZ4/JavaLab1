e5a7dba5fd5ffce84f591562c38dc5591d495705
Juan Buitrago <buitr4go@gmail.com>
2022-06-08 16:40:48 -0700

    JavaLab

---
+++ Exercise1/src/Exercise_4.java
@@ -0,0 +1,9 @@
+public class Exercise_4 {
+    public static void main(String[] args){
+        System.out.printf("%4S%4s%s%3s%s%5s%s%2s%s%n","j","","a","","v","","v","","a");
+        System.out.printf("%4S%4s%2s%4s%4s%3s%2s%n","j","a","a","v","v","a","a");
+        System.out.printf("%S%3S%2s%s%4S%2S%2s%s%n","j","j","","aaaaa","v","v","","aaaaa");
+        System.out.printf("%S%2s%6s%4S%3s%6s%n"," jj ","a","a","v","a","a");
+
+    }
+}
